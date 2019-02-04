d99d47fa481d97e4519a6e91d2677f3d79e8d57d
alexanderutrobin11 <alexanderutrobin11@gmail.com>
2019-02-04 09:05:42 +0300

    Updates

---
+++ precache-manifest.e7b0dc6fbcf20c55815c29340450d58a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cfc7846801705b928144dc9408ad57f",
+    "url": "/fun-box-app/static/media/CloseButton.3cfc7846.svg"
+  },
+  {
+    "revision": "0cf228e91bc3a613c291",
+    "url": "/fun-box-app/static/js/runtime~main.0cf228e9.js"
+  },
+  {
+    "revision": "faefcc383bfa035303ee",
+    "url": "/fun-box-app/static/js/main.faefcc38.chunk.js"
+  },
+  {
+    "revision": "6f75c65993b8a1ae8c66",
+    "url": "/fun-box-app/static/js/1.6f75c659.chunk.js"
+  },
+  {
+    "revision": "faefcc383bfa035303ee",
+    "url": "/fun-box-app/static/css/main.b5ec886b.chunk.css"
+  },
+  {
+    "revision": "81e0e97daf401b7edda8fdf7a3764da3",
+    "url": "/fun-box-app/index.html"
+  }
+];
